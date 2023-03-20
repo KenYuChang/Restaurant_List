cc9c4d807d54136a028407960d428644830f9d4a
KenYuChang <justin0915justin0915@gmail.com>
2023-03-20 15:34:59 +0800

    feat: add todo seeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,30 +1,21 @@
-const express = require("express");
-const app = express();
-const port = 3000;
 const mongoose = require("mongoose");
-
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+const Todo = require("../todo"); // 載入 todo model
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}); //設定連線到mongoDB
-
+});
 const db = mongoose.connection;
-
 db.on("error", () => {
   console.log("mongodb error!");
 });
-
 db.once("open", () => {
   console.log("mongodb connected!");
-});
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
-
-app.listen(port, (req, res) => {
-  console.log(`App is running on http://localhost:${port}`);
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: `name-${i}` });
+  }
+  console.log("done");
 });
