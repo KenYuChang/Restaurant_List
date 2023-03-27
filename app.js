9bec572b7910f884fcf76c957791074426ba4c98
KenYuChang <justin0915justin0915@gmail.com>
2023-03-27 16:56:02 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 const express = require("express");
-const app = express();
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
-const port = 3000;
-const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-const Todo = require("./models/todo");
+
 const routes = require("./routes");
+require("./config/mongoose");
+
+const app = express();
+const port = 3000;
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}); //設定連線到mongoDB
 
-const db = mongoose.connection;
-
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
