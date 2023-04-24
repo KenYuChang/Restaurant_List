2885694d9ebddabd03088aca1a40ac9f56db8cde
KenYuChang <justin0915justin0915@gmail.com>
2023-04-24 16:04:59 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
-
+const session = require("express-session");
 const routes = require("./routes");
 require("./config/mongoose");
 
@@ -12,6 +12,13 @@ const port = 3000;
 //setting template engine 設置樣版引擎
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.use(
+  session({
+    secret: "ThisIsMySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
