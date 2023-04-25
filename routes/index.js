37e46faafd068ec2baf4124772ac00dda1a0f025
KenYuChang <justin0915justin0915@gmail.com>
2023-04-25 17:57:01 +0800

    feat: add fb login and dotenv

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const home = require("./modules/home");
 const restaurant = require("./modules/restaurant");
 const users = require("./modules/users");
+const auth = require("./modules/auth");
 const { authenticator } = require("../middleware/auth"); // middleware
 
 router.use("/restaurants", authenticator, restaurant); //驗證
 router.use("/users", users);
+router.use("/auth", auth);
 router.use("/", authenticator, home); //驗證
 
 module.exports = router;
